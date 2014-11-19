abcc6ddbfffdb0629f64ac3fe065f630f4183697
leonardo <leonardo@babe9f55-0b82-4839-ba80-d03984e3c3df>
2014-11-19 03:45:23 +0000

    Improved for Frame len >1 in presence of  FEC blocks . It doesn't still work for FEC on and FRame len > 1 for latency problems. Generilized the function for adding and/or removing dinamically blocks

--- trunk/Matlab/Leo_imp3/WM_Sim_config_smlnk_standalone.m
+++ trunk/Matlab/Leo_imp3/WM_Sim_config_smlnk_standalone.m
@@ -6,27 +6,30 @@ clear all;close all;clc
 matlab_sim=false;
 
 %%%%SIM PARAMS
+fec                         = 'on';
+fec_k                   = 7;%9;%5;
+fec_n                   = 63;%255;%15;
 
-Approx_char_rate            = 10;%coded effective to bedivided by fec ratio
+Approx_fec_byte_rate            = 10;%coded effective to bedivided by fec ratio
+fprintf('*********Approximative char rate at source is %f\n', Approx_fec_byte_rate*fec_k/fec_n);
 Time_length                 = 40;
 Carrier_Freq                = 500 ;
 hopping_approx_frequency    = 200;
 hopping                     = 'off';
 suppression_filter          = 'off';
 supp_filt_order             = 8;
-Eb_N0_dB                    = 0;
-Frame_len                   = 1;
+Eb_N0_dB                    = 20;
+Frame_len                   = 2;
 Tx_approx_BW                = 9e3;
-fec                         = 'on';
-fec_k                   = 9;%5;
-fec_n                   = 255;%15;
 
 
 save_before_close = 1;
 
   close_system('WM_Sim',save_before_close);
 
-WM=WM_Sim_config(Time_length,Approx_char_rate,Tx_approx_BW,Carrier_Freq,hopping_approx_frequency,...
+WM=WM_Sim_config(Time_length,Approx_fec_byte_rate,Tx_approx_BW,Carrier_Freq,hopping_approx_frequency,...
                                       hopping,Frame_len,suppression_filter,supp_filt_order,Eb_N0_dB,'noise',fec,fec_k,fec_n);
 
  open_system('WM_Sim');
+ 
+ fprintf('**********Effective char rate at source is %f\n', WM.Sim.Encoder.Char_Rate);
